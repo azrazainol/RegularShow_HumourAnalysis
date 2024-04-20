1427da078e01892318d847050ee70a4682a3ce69
MacBook Air <macbookair@Azras-MacBook-Air.local>
2024-04-20 21:30:13 +0800

    analysis

--- Assignment_1.R
+++ Assignment_1.R
@@ -26,6 +26,8 @@ library(RColorBrewer)
 library(syuzhet)
 library(ggplot2)
 library(stringr)
+library(tidyverse)
+library(tokenizers)
 
 #import txt file
 script1 <- readLines("regularshow_season1_20240418_raw.txt")
@@ -48,7 +50,7 @@ for (line in script1) {
 #splitting words/sentences
 sentences <- str_extract_all(script, "\\b\\w+\\b.*?[.!?](?=\\s|$)")
 sentences <- unlist(sentences)
-words <- unlist(str_split(sentences, "\\s+"))
+################## words <- unlist(str_split(sentences, "\\s+"))
 
 #convert script (chr) to Corpus
 regshow <- Corpus(VectorSource(sentences))
@@ -56,31 +58,64 @@ class(regshow)
 inspect(regshow)
 
 #clean
-#toSpace <- content_transformer(function(x, pattern)
-#  gsub(pattern,"", x))
+toSpace <- content_transformer(function(x, pattern)
+  gsub(pattern,"", x))
 
 regshow1 <- regshow
 regshow1 <- tm_map(regshow1, content_transformer(tolower))
 regshow1 <- tm_map(regshow1, stripWhitespace)
+regshow1 <- tm_map(regshow1, removeNumbers)
+regshow1 <- tm_map(regshow1, toSpace, "♪")
+regshow1 <- tm_map(regshow1, toSpace, "’")
 
 regshow2 <- regshow1
-regshow2 <- tm_map(regshow1, removeNumbers)
-regshow2 <- tm_map(regshow1, removeWords, stopwords("english"))
-regshow2 <- tm_map(regshow1, removePunctuation)
+regshow2 <- tm_map(regshow2, removeWords, stopwords("english"))
+regshow2 <- tm_map(regshow2, stemDocument) #text stemming for sentiment & wordcloud
+regshow2 <- tm_map(regshow2, removePunctuation)
 
+regshow1 <- tm_map(regshow1, removePunctuation)
+
+#convert Corpus to chr
 regshow1_ch <- sapply(regshow1, as.character)
 regshow2_ch <- sapply(regshow2, as.character)
+
+#create sentences vector and words vector
+sentences <- regshow1_ch
+words <- unlist(str_split(sentences, "\\s+"))
+
+#tokenize
+regshow_tokens <- tokenize_words(sentences)
 
 
 ## 3. analysis - explain analysis in README.md file and save analysis components in a folder
 
+# Wordcloud
+dtm <- TermDocumentMatrix(regshow2)
+regshow2_dtm <- as.matrix(dtm)
+regshow2_sort <- sort(rowSums(regshow2_dtm), decreasing = T)
+regshow2_df <- data.frame(word = names(regshow2_sort), freq = regshow2_sort)
+
+wordcloud(words = regshow2_df$word, regshow2_df$freq, min.freq = 5,
+          random.order = F, colors = brewer.pal(8, "Dark2"))
+
+# Sentiment Analysis
+library(sentimentr)
+regshow_sentiment <- get_sentiment(regshow1_ch, method = "syuzhet")
+mean(regshow_sentiment)
+hist(regshow_sentiment)
+summary(regshow_sentiment)
+
+
+# Emotion Classification
+regshow_ec <- get_nrc_sentiment(regshow1_ch)
+regshow_ec_df <- data.frame(t(regshow_ec))
+regshow_ec_sums <- data.frame(rowSums(regshow_ec_df))
+regshow_ec_sums
+
+
 # Vocabulary Richness: Calculate type-token ratio (TTR)
 unique_words <- unique(words)
 ttr <- length(unique_words) / length(words)
-
-
-num_unique_words <- length(unique(tolower(words(corpus))))
-
 
 # Sentence Length: Calculate average sentence length
 sentence_lengths <- sapply(sentences, function(x) length(unlist(str_split(x, "\\s+"))))
@@ -125,6 +160,10 @@ flesch_kincaid_grade_level <- 0.39 * words_per_sentence + 11.8 * syllables_per_w
 
 
 
-
-
+############## what to do:
+# get season 2 scripts or another cartoon network show (adventure time) script
+# do comparison analysis?
+### or
+# get an adult show script
+# do comparison analysis
 
